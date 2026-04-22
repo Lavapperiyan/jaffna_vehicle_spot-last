660dbec69ab5703f454c19e73a5e1bd6a04bba22
Lavapperiyan <lavapperiyan@gmail.com>
2026-04-22 13:21:55 +0530

    Disable auto-login and force LoginScreen on app start

--- jaffna_vehicle_spot/lib/main.dart
+++ jaffna_vehicle_spot/lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         textTheme: GoogleFonts.interTextTheme(),
       ),
-      home: AuthService().userId.isEmpty ? const LoginScreen() : const MainLayout(),
+      home: const LoginScreen(),
     );
   }
 }
